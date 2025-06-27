f57efb4dbd8f6d8053a01830026283743abf193b
malya srivastava <malyasri0112@gmail.com>
2025-06-27 23:54:19 +0530

    form handling with actions

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,27 +1,47 @@
+import { useState } from "react";
+
 export default function Main() {
+  const [ingredients, setAddIngredients] = useState([]);
 
-  const ingredients = []
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log("form submit");
-    const formData = new FormData(e.currentTarget);
+  function handleSubmit(formData) {
     const newIngredient = formData.get("ingredient");
-    ingredients.unshift(newIngredient)
-    console.log(newIngredient);
-    console.log(ingredients)
+    if (newIngredient) {
+      setAddIngredients([newIngredient, ...ingredients]);
+    }
   }
+
+  const ingredientListItems = ingredients.map((ingredient, i) => <li key={i}>{ingredient}</li>);
+  console.log(ingredientListItems)
+  console.log(typeof(ingredientListItems))
+
+
   return (
     <main>
-      <form className="search-section" onSubmit={handleSubmit}>
+      <form className="search-section" action={handleSubmit}>
         <input
           type="text"
           placeholder="e.g. oregano"
           aria-label="Add Ingredient"
           name="ingredient"
         />
-        <button>+ Add Ingredient</button>
+        <button type="submit">+ Add Ingredient</button>
       </form>
+      <div className="ingredient-list-section">
+        <h1>Ingredients on Hand:</h1>
+
+        {ingredients.length > 0 && <ul className="ingredient-list">{ingredientListItems}</ul>}
+
+        {
+          ingredients.length > 0 &&
+          <div className="get-recipe">
+          <div>
+            <h3>Ready for a recipe?</h3>
+            <p>Generate a recipe from your list of ingredients.</p>
+          </div>
+          <button>Get a recipe</button>
+          </div>
+        }
+      </div>
     </main>
   );
 }
