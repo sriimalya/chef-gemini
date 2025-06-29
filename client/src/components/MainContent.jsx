6b0c0e2bf7ea01f79de0da76eac1ac91410bfab3
malya srivastava <malyasri0112@gmail.com>
2025-06-29 22:02:20 +0530

    url update

--- client/src/components/MainContent.jsx
+++ client/src/components/MainContent.jsx
@@ -18,7 +18,7 @@ export default function Main() {
   async function getRecipe() {
     setLoading(true);
     try {
-      const res = await fetch("http://localhost:5000/get-recipe", {
+      const res = await fetch("https://chef-gemini.onrender.com", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
