631a8558672a0115d372d5bdc1f5c2fb9d0b4e50
malya srivastava <malyasri0112@gmail.com>
2025-07-03 23:11:31 +0530

    improved ui

--- client/src/components/IngredientList.jsx
+++ client/src/components/IngredientList.jsx
@@ -1,11 +1,11 @@
-import { Trash2 } from 'lucide-react';
+import { X } from 'lucide-react';
 
 export default function IngredientList({ ingredients, getRecipe, removeIngredient }) {
   const ingredientListItems = ingredients.map((ingredient) => (
     <li key={ingredient.id}>
       <div className='list-content'>
         <span className='ingredient-name'>{ingredient.name}</span>  
-        <button className='remove-icon' onClick={()=>removeIngredient(ingredient.id)}><Trash2 size={20} /></button>
+        <button className='remove-icon' onClick={()=>removeIngredient(ingredient.id)}><X/></button>
       </div>
     </li>
   ));
@@ -16,14 +16,14 @@ export default function IngredientList({ ingredients, getRecipe, removeIngredien
         <h1>Ingredients on Hand:</h1>
 
         {ingredients.length > 0 ? (
-          <>
-            <ul className="ingredient-list">{ingredientListItems}</ul>
+          <div className='ingredient-list'>
+            <ul>{ingredientListItems}</ul>
             {ingredients.length < 3 && (
               <p className="not-enough-ingredient">
                 *add at least 3 ingredients to generate a recipe.
               </p>
             )}
-          </>
+          </div>
         ) : (
           <p>You've not added any ingredients yet.</p>
         )}
