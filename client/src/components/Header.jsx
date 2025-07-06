e126a6dbd0fb040ea55825e032296ac4aa9167da
malya srivastava <malyasri0112@gmail.com>
2025-07-06 23:58:40 +0530

    modified header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,18 @@
 import ChefIcon from '../assets/chef-icon.png'
+import {HistoryIcon, Bookmark} from 'lucide-react'
 
 function Header() {
   return (
     <header>
+      <div className='left-item'>
         <img src={ChefIcon} alt="logo"/>
         <p>Chef Gemini</p>
+      </div>
+      <div className='right-item'>
+        <HistoryIcon/>
+        <Bookmark/> 
+        <button className='user-icon'></button>       
+      </div>
     </header>
   )
 }
