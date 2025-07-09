2f980c57354c115c88285fa5085707df17873b7c
malya srivastava <malyasri0112@gmail.com>
2025-07-09 09:32:11 +0530

    fix: cors

--- server/index.js
+++ server/index.js
@@ -16,6 +16,17 @@ const allowedOrigins = [
   "http://localhost:5173",
   "https://chef-gemini-iota.vercel.app"
 ];
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+}));
 app.options("*", cors({
   origin: allowedOrigins,
   credentials: true,
